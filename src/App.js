3ee3ce6a2a5b5eafd734a1e108d2fc489f6463fc
peteragnus <prabhuchira@gmail.com>
2021-01-29 18:22:19 +0530

    added files

---
+++ src/App.js
@@ -0,0 +1,134 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import { GoogleSignin, GoogleSigninButton } from '@react-native-community/google-signin';
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Platform,
+} from 'react-native';
+
+
+
+
+const App =()=>{
+
+  return(
+    <>
+      <SafeAreaView style={{flex:1}}>
+        <ScrollView style={{flex:1}}>
+            <Text>Levels</Text>
+            <View style={{flex:1}}>
+
+              {
+                Platform.OS=="web" ?
+                null:
+                <GoogleSigninButton>
+
+                </GoogleSigninButton>
+              }
+            
+            </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  )
+}
+
+
+// const App =()=> {
+//   return (
+//     <>
+//       <StatusBar barStyle="dark-content" />
+//       <SafeAreaView>
+//         <ScrollView
+//           contentInsetAdjustmentBehavior="automatic"
+//           style={styles.scrollView}>
+       
+//           {global.HermesInternal == null ? null : (
+//             <View style={styles.engine}>
+//               <Text style={styles.footer}>Engine: Hermes</Text>
+//             </View>
+//           )}
+//           <View style={styles.body}>
+//             <View style={styles.sectionContainer}>
+//               <Text style={styles.sectionTitle}>Step One</Text>
+//               <Text style={styles.sectionDescription}>
+//                 Edit <Text style={styles.highlight}>App.js</Text> to change this
+//                 screen and then come back to see your edits.
+//               </Text>
+//             </View>
+//             <View style={styles.sectionContainer}>
+//               <Text style={styles.sectionTitle}>See Your Changes</Text>
+//               <Text style={styles.sectionDescription}>
+//               Hello World!
+//               </Text>
+//             </View>
+//             <View style={styles.sectionContainer}>
+//               <Text style={styles.sectionTitle}>Debug</Text>
+//               <Text style={styles.sectionDescription}>
+//                 {/* <DebugInstructions /> */}
+//               </Text>
+//             </View>
+//             <View style={styles.sectionContainer}>
+//               <Text style={styles.sectionTitle}>Learn More</Text>
+//               <Text style={styles.sectionDescription}>
+//                 Read the docs to discover what to do next:
+//               </Text>
+//             </View>
+//             {/* <LearnMoreLinks /> */}
+//           </View>
+//         </ScrollView>
+//       </SafeAreaView>
+//     </>
+//   );
+// };
+
+const styles = StyleSheet.create({
+  scrollView: {
+   
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+ 
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+   
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
